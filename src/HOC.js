61148468f4f3338149a0f1cd39b40aea1abc9b0a
Suraj-subin <surajsubin5@gmail.com>
2023-09-29 10:33:15 +0545

    HOC in react js

--- src/HOC.js
+++ src/HOC.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 
-// In React, a higher-order component is a function that takes a component as an argument and returns a new component
+// In React, a higher-order component is a function that takes a component as an argument and returns a new component.
 // Higher Order Component vitra functional component halnu parxa.
 
 const HigherOrderComponent = (Student) => {
   function HigherOrderComponentInner() {
-    // HigherOrderComponentInner is Functional Component.
+    // HigherOrderComponentInner yo  Functional Component ho.
     const [num, setNum] = useState(0);
 
     const handleEvent = () => {
